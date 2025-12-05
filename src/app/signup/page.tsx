8a956e61510d68ee25c88ac58c2ebb4a33288d41
racheluwera <racheluwera9@gmail.com>
2025-12-05 09:55:17 +0200

    initial commit

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function SignupPage() {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+}
+
+    
