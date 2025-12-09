f96ce04f6b256465f65987e8ff6ee51341708535
Bonifilda <bonimani2020@gmail.com>
2025-12-09 15:21:30 +0200

    added other user in dummy data

--- src/lib/dummy/users.ts
+++ src/lib/dummy/users.ts
@@ -2,13 +2,25 @@ export const DUMMY_USERS = [
   {
     id: 1,
     name: "Boni User",
-    email: "boni@example.com",
+    email: "boni@gmail.com",
     password: "123456",
   },
   {
     id: 2,
     name: "Admin",
-    email: "admin@example.com",
+    email: "admin@gmail.com",
     password: "admin123",
   },
+  {
+    id:3,
+    name:"peter User",
+    email:"peter@gmail.com",
+    password:"peter123",
+  },
+  {id:4,
+    name:"damma",
+    email:"damma@gmail.com",
+    password:"dama123",
+  }
 ];
+
